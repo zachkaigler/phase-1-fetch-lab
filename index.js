f434de97fa6c17e9f726b81a476aba8be8ab7dac
Zach Kaigler <zachkaiglerfilm@gmail.com>
2021-03-21 16:37:06 -0400

    commit tree 155d86a6e911ab8ee07b985bf954be04c785468d

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  return fetch('https://anapioficeandfire.com/api/books')
+  .then(function(response) {
+    return response.json()
+  }).then(function(json) {
+    renderBooks(json)
+  })
 }
 
 function renderBooks(books) {
